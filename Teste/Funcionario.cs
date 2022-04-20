cb9d96710d16e57225a8a1514e2fab51b3940dca
Marcus Vinicius Lima da Costa <marcusv2001@gmail.com>
2022-04-20 18:14:56 -0300

    Teste

---
+++ Teste/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    internal class Funcionario
+    {
+        public int id;
+        public string nome;
+        public int salario;
+        public Funcionario(int id, string nome, int salario){
+           this.id = id;
+           this.nome = nome;
+           this.salario = salario;
+        }
+
+        public void AumentarSalario(int porcentagem)
+        {
+            salario += salario * porcentagem / 100;
+        }
+        public override string ToString()
+        {
+            return "\nid = " + id
+                    + "\nnome = " + nome
+                    + "\nsalario = " + salario + "\n";
+        }
+    }
+}
---
+++ Teste/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int TAM = 2;
+            int[,] matriz = new int[TAM,TAM];
+            int negativos = 0;
+            for(int i = 0; i < TAM; i++)
+            {
+                for(int j = 0; j < TAM; j++)
+                {
+                    matriz[i,j] = int.Parse(Console.ReadLine());
+                    if (matriz[i,j] <= 0)
+                        negativos += 1;
+                }
+            }
+            Console.WriteLine("\nExistem " + negativos + " números negativos na matriz");
+            Console.Write("\nA diagonal da matriz é composta pelos números : ");
+            for(int i = 0; i < TAM; i++)
+            {
+                Console.Write("["+ matriz[i,i]+"]");
+            }
+        }
+    }
+}
